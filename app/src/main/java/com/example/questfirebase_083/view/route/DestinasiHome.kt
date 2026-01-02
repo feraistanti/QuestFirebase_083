2bbdce6a9b65dc46d487f64eb691697edf0a27c6
feraistanti <fistanti551@gmail.com>
2026-01-02 21:38:35 +0700

    Nambahin route halaman utama

---
+++ app/src/main/java/com/example/questfirebase_083/view/route/DestinasiHome.kt
@@ -0,0 +1,10 @@
+package com.example.questfirebase_083.view.route
+
+
+import com.example.QuestFirebase.R
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
