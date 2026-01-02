e283f2c9686e33b7ddf16cb2c6560f7a50bd5258
feraistanti <fistanti551@gmail.com>
2026-01-02 21:31:00 +0700

    Nambahin route tambah siswa

---
+++ app/src/main/java/com/example/questfirebase_083/view/route/DestinasiEntry.kt
@@ -0,0 +1,10 @@
+package com.example.questfirebase_083.view.route
+
+
+import com.example.QuestFirebase.R
+
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
