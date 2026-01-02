2c5e3ecfe1caf320a6c7384532d3eef1a8d827b7
feraistanti <fistanti551@gmail.com>
2026-01-02 21:23:14 +0700

    Nambahin route detail siswa

---
+++ app/src/main/java/com/example/questfirebase_083/view/route/DestinasiDetail.kt
@@ -0,0 +1,12 @@
+package com.example.questfirebase_083.view.route
+
+
+import com.example.QuestFirebase.R
+
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
