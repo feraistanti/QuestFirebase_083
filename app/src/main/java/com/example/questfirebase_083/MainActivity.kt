3185e31c8954049d05b720bd46cc500b195dfc02
feraistanti <fistanti551@gmail.com>
2026-01-02 21:15:18 +0700

    Activity utama, pusat kendali aplikasi

---
+++ app/src/main/java/com/example/questfirebase_083/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.questfirebase_083
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.questfirebase_083.ui.theme.QuestFirebase_083Theme
+import com.example.questfirebase_083.view.ControlNavigasi.DataSiswaApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            // Sesuaikan nama tema dengan yang ada di folder ui.theme/Theme.kt
+            QuestFirebase_083Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // Pastikan composable DataSiswaApp sudah dibuat (biasanya berisi NavHost)
+                    DataSiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
