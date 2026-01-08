ffa95dd0156bfbf665f3f88a60bd48bbb497b859
feraistanti <fistanti551@gmail.com>
2026-01-08 16:02:38 +0700

    Navigasi ke halaman tambah siswa

--- app/src/main/java/com/example/questfirebase_083/view/ControlNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/questfirebase_083/view/ControlNavigasi/PetaNavigasi.kt
@@ -50,3 +50,11 @@ fun HostNavigasi(
             )
         }
 
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+
