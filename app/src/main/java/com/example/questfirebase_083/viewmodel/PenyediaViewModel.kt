59605192322e9fb18c7961d1a6088c5d933ad1b5
feraistanti <fistanti551@gmail.com>
2026-01-08 16:06:19 +0700

    Daftar ViewModel yang menggunakan repository siswa

--- app/src/main/java/com/example/questfirebase_083/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questfirebase_083/viewmodel/PenyediaViewModel.kt
@@ -1,17 +1,46 @@
 package com.example.questfirebase_083.viewmodel
 
-import androidx.lifecycle.ViewModelProvider
+
+
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questfirebase_083.repositori.AplikasiDataSiswa
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]
+            as AplikasiDataSiswa)
 
 object PenyediaViewModel {
+
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
-        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+
+        initializer {
+            HomeViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+
+        initializer {
+            EntryViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
     }
 }
